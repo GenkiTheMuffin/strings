f25d62e18c99a23304d72071e5e3e807d081188b
GenkiTheMuffin <owlysk@gmail.com>
2023-12-01 13:06:34 +0100

    .

--- caesar-ciphre/main.py
+++ caesar-ciphre/main.py
@@ -1,1 +1,22 @@
-def caesar_ciphre(s:str,shift:int)
+def caesar_ciphre_encode(s: str, shift: int):
+    encoded_string = ""
+    for i in s:
+        ordinal_value = ord(i)
+        ordinal_value = (ordinal_value - 97 + shift) % 26 + 97
+        encoded_character = chr(ordinal_value)
+        encoded_string += encoded_character
+    return encoded_string
+
+
+def caesar_ciphre_decode(s: str, shift: int):
+    decoded_string = ""
+    for i in s:
+        ordinal_value = ord(i)
+        ordinal_value = (ordinal_value - 97 - shift) % 26 + 97
+        decoded_character = chr(ordinal_value)
+        decoded_string += decoded_character
+    return decoded_string
+
+
+print(caesar_ciphre_encode("ahoj", 29))
+print(caesar_ciphre_decode("dkrm", 29))
